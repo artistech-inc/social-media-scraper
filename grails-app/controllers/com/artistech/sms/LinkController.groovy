06d46a0b59fb750057bb40e2f0324e6cffa8bc1b
Matthew Aguirre <matt.aguirre@gmail.com>
2017-11-18 12:40:03 -0500

    Update on get lists

--- grails-app/controllers/com/artistech/sms/LinkController.groovy
+++ grails-app/controllers/com/artistech/sms/LinkController.groovy
@@ -7,6 +7,12 @@ class LinkController {
 
     static scaffold = Link
 
+    def index() {
+        long offset = params.offset == null ? 0 : params.offset as long
+        int max = params.max == null ? 10 : params.max as int
+        [linkList: Link.list(offset: offset, max: max), linkCount: Link.count()]
+    }
+
     def show() {
         Link link = Link.get(params.id)
         respond ([url: link.url, resolved: link.resolved, tweetid: link.tweet.id_str, tweetdbid: link.tweet.id, downloaded: link.contents != null])
--- grails-app/controllers/com/artistech/sms/TweetUserController.groovy
+++ grails-app/controllers/com/artistech/sms/TweetUserController.groovy
@@ -4,4 +4,10 @@ class TweetUserController {
 
     static scaffold = TweetUser
 
+    def index() {
+        long offset = params.offset == null ? 0 : params.offset as long
+        int max = params.max == null ? 10 : params.max as int
+        [tweetUserList: TweetUser.list(offset: offset, max: max), tweetUserCount: TweetUser.count()]
+    }
+
 }
